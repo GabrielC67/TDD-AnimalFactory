a52866d4b6da7a9df500037695c5168651fb4c62
GabrielC67 <gabecruzpro@gmail.com>
2025-05-19 23:02:46 -0400

    Sending to Mac

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,5 +1,12 @@
 package rocks.zipcodewilmington;
 
+import rocks.zipcodewilmington.animals.Animal;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
@@ -7,7 +14,16 @@ public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
 
+    @Test
     public void createDogTest(){
+        Date givenBirthDate = new Date();
+        AnimalFactory factory = new AnimalFactory();
+        Dog dog = new Dog("", givenBirthDate, null);
 
+        Dog newDog = factory.createDog("Codi", givenBirthDate);
+
+        Assert.assertEquals(dog.getName(), newDog.getName());
+        Assert.assertEquals(dog.getBirthDate(), newDog.getBirthDate());
+        Assert.assertEquals(dog.getId(), newDog.getId());
     }
 }
--- src/test/java/rocks/zipcodewilmington/DogHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/DogHouseTest.java
@@ -1,9 +1,6 @@
 package rocks.zipcodewilmington;
 
-import org.junit.Test;
-import rocks.zipcodewilmington.animals.Dog;
-import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
-import rocks.zipcodewilmington.animals.animal_storage.DogHouse;
+
 
 import java.util.Date;
 
@@ -17,18 +14,5 @@ public class DogHouseTest {
     // TODO - Create tests for `Dog getDogById(Integer id)`
     // TODO - Create tests for `Integer getNumberOfDogs()`
 
-    @Test
-    public void testGetNumberOfDogs() {
-        // Given (some
-        String name = "Milo";
-        Date birthDate = new Date();
-        Dog animal = AnimalFactory.createDog(name, birthDate);
-        DogHouse.clear();
 
-        // When
-        DogHouse.add(animal);
-
-        // Then
-        DogHouse.getNumberOfDogs();
-    }
 }
