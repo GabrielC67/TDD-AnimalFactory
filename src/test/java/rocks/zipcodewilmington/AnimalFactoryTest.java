4ceb14a5d0f1b1636b1d614233d9fb7ac1c23d55
GabrielC67 <gabecruzpro@gmail.com>
2025-05-21 11:38:20 -0400

    AnimalFactoryTest Completed

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,6 +1,9 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
 import rocks.zipcodewilmington.animals.Animal;
+import rocks.zipcodewilmington.animals.Cat;
 import rocks.zipcodewilmington.animals.Dog;
 import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
 
@@ -18,12 +21,25 @@ public class AnimalFactoryTest {
     public void createDogTest(){
         Date givenBirthDate = new Date();
         AnimalFactory factory = new AnimalFactory();
-        Dog dog = new Dog("", givenBirthDate, null);
+        Dog expectedDog = new Dog("Codi", givenBirthDate, 0);
 
         Dog newDog = factory.createDog("Codi", givenBirthDate);
 
-        Assert.assertEquals(dog.getName(), newDog.getName());
-        Assert.assertEquals(dog.getBirthDate(), newDog.getBirthDate());
-        Assert.assertEquals(dog.getId(), newDog.getId());
+        Assert.assertEquals(expectedDog.getName(), newDog.getName());
+        Assert.assertEquals(expectedDog.getBirthDate(), newDog.getBirthDate());
+        Assert.assertEquals(expectedDog.getId(), newDog.getId());
     }
+    @Test
+    public void createCatTest(){
+        Date givenBirthDate = new Date();
+        AnimalFactory factory = new AnimalFactory();
+        Cat expectedCat = new Cat("Peanut", givenBirthDate, 0);
+
+        Cat newCat = factory.createCat("Peanut", givenBirthDate);
+
+        Assert.assertEquals(expectedCat.getName(), newCat.getName());
+        Assert.assertEquals(expectedCat.getBirthDate(), newCat.getBirthDate());
+        Assert.assertEquals(expectedCat.getId(), newCat.getId());
+    }
+
 }
