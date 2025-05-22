c335e3178fe29214efebea3d2a2070b132aa5ea4
GabrielC67 <gabecruzpro@gmail.com>
2025-05-21 23:34:50 -0400

    Corrected any errors from list.

--- src/test/java/rocks/zipcodewilmington/CatHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/CatHouseTest.java
@@ -26,6 +26,7 @@ public class CatHouseTest {
 
     @Test
     public void testAddCat(){
+        setup();
         Cat testCat = new Cat ("Joey", new Date(), 1);
 
         CatHouse.add(testCat);
@@ -35,17 +36,19 @@ public class CatHouseTest {
 
     @Test
     public void testRemoveCatById(){
+        setup();
         Cat testCat = new Cat("Jojo", new Date(), 2);
 
         CatHouse.add(testCat);
         CatHouse.remove(2);
-        Cat retrieved = CatHouse.getCatById(3);
+        Cat retrieved = CatHouse.getCatById(2);
 
         Assert.assertNull(retrieved);
     }
 
     @Test
     public void testRemoveCatObject(){
+        setup();
         Cat testCat = new Cat("Moogi", new Date(), 3);
 
         CatHouse.add(testCat);
@@ -57,6 +60,7 @@ public class CatHouseTest {
 
     @Test
     public void testGetCatById(){
+        setup();
         Cat testCat = new Cat("Momo", new Date(), 4);
 
         CatHouse.add(testCat);
@@ -66,7 +70,8 @@ public class CatHouseTest {
     }
 
     @Test
-    public void getNumberOfCatsTest(){
+    public void getNumberOfCatsTest1(){
+        setup();
         Cat testCat1 = new Cat("Simba", new Date(), 5);
         Cat testCat2 = new Cat("Nala", new Date(), 6);
         Cat testCat3 = new Cat("Scar", new Date(), 7);
@@ -82,4 +87,16 @@ public class CatHouseTest {
         Assert.assertEquals(4, numberOfCats);
     }
 
+    @Test
+    public void getNumberOfCatsTest2() {//The difference is there's no reference variable
+        setup();
+        CatHouse.add(new Cat("Simba", new Date(), 5));
+        CatHouse.add(new Cat("Nala", new Date(), 6));
+        CatHouse.add(new Cat("Scar", new Date(), 7));
+        CatHouse.add(new Cat("Mufasa", new Date(), 8));
+
+        int numberOfCats = CatHouse.getNumberOfCats();
+
+        Assert.assertEquals(4, numberOfCats);
+    }
 }
